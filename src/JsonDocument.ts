de20e71774e7be3c15c4dac10a5bedd1df71918e
Antoon Beres <antoonberes@hotmail.com>
2023-11-21 13:36:27 +0100

    initial commit after fork

--- src/JsonDocument.ts
+++ src/JsonDocument.ts
@@ -1,62 +1,65 @@
-import { Edm } from 'odata-v4-metadata'
-import * as extend from 'extend'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Edm } from 'odata4-metadata';
+import * as extend from 'extend';
 
-var containsField = (obj, field, cb) => {
-    if (obj && field in obj && typeof obj[field] !== "undefined") {
-        cb(obj[field])
-    }
-}
+const containsField = (obj, field, cb) => {
+  if (obj && field in obj && typeof obj[field] !== 'undefined') {
+    cb(obj[field]);
+  }
+};
 
 export class JsonDocument {
-    public metadata: Edm.Edmx
-    private options: any
+  public metadata: Edm.Edmx;
+  private options: any;
 
-    constructor(options: any, edmx: Edm.Edmx) {
-        options = options || {};
+  constructor(options: any, edmx: Edm.Edmx) {
+    options = options || {};
 
-        this.options = extend({}, options)
+    this.options = extend({}, options);
 
-        this.metadata = edmx
-    }
+    this.metadata = edmx;
+  }
 
+  processMetadata() {
+    const context = {};
+    const json = {
+      '@odata.context': this.options.context,
+      value: [],
+    };
+    this.buildEdmx(json, this.metadata, context);
+    return json;
+  }
 
-    processMetadata() {
-        var context = {};
-        var json = {
-            '@odata.context': this.options.context,
-            value: []
-        };
-        this.buildEdmx(json, this.metadata, context);
-        return json;
-    }
+  buildEdmx(json, edmx, context) {
+    this.buildDataServices(json, edmx.dataServices, context);
+  }
 
-    buildEdmx(json, edmx, context) {
-        this.buildDataServices(json, edmx.dataServices, context);
-    }
+  buildDataServices(json, dataservices, context) {
+    this.buildSchema(json, dataservices.schemas, context);
+  }
 
-    buildDataServices(json, dataservices, context) {
-        this.buildSchema(json, dataservices.schemas, context);
-    }
+  buildSchema(json, schemas, context) {
+    schemas &&
+      schemas.forEach((schema) => {
+        this.buildEntityContainer(json, schema.entityContainer, context);
+      });
+  }
 
-    buildSchema(json, schemas, context) {
-        schemas && schemas.forEach(schema => {
-            this.buildEntityContainer(json, schema.entityContainer, context);
-        })
-    }
-    
-    buildEntityContainer(json, entityContainers, context) {
-        entityContainers && entityContainers.forEach(entityContainer => {
-            this.buildEntitySets(json, entityContainer.entitySets, context)
-        })
-    }
-    
-    buildEntitySets(json, entitySets, context) {
-        entitySets && entitySets.forEach(entitySet => {
-            json.value.push({
-                name: entitySet.name,
-                kind: "EntitySet",
-                url: entitySet.name,
-            })
-        })
-    }
+  buildEntityContainer(json, entityContainers, context) {
+    entityContainers &&
+      entityContainers.forEach((entityContainer) => {
+        this.buildEntitySets(json, entityContainer.entitySets, context);
+      });
+  }
+
+  buildEntitySets(json, entitySets, context) {
+    entitySets &&
+      entitySets.forEach((entitySet) => {
+        json.value.push({
+          name: entitySet.name,
+          kind: 'EntitySet',
+          url: entitySet.name,
+        });
+      });
+  }
 }
--- src/serviceDocument.ts
+++ src/serviceDocument.ts
@@ -1,38 +1,40 @@
-import { Edm } from 'odata-v4-metadata'
-import { ServiceMetadata } from 'odata-v4-service-metadata'
-import { JsonDocument } from './JsonDocument'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Edm } from 'odata4-metadata';
+import { ServiceMetadata } from 'odata4-service-metadata';
+import { JsonDocument } from './JsonDocument';
 import { Request, Response, RequestHandler } from 'express';
 
 export class ServiceDocument extends ServiceMetadata {
-    constructor(edmx: Edm.Edmx, options?: Object) {
-        super(edmx, options);
-    }
+  constructor(edmx: Edm.Edmx, options?: object) {
+    super(edmx, options);
+  }
 
-    document(format?: string) {
-        switch (format) {
-            case 'xml':
-                throw new Error('Not implemented');
-            default: return this.data;
-        }
+  document(format?: string) {
+    switch (format) {
+      case 'xml':
+        throw new Error('Not implemented');
+      default:
+        return this.data;
     }
+  }
 
-    process(edmx: Edm.Edmx, options?: Object) {
-        var jsonDocument = new JsonDocument(options, edmx);
-        this.data = jsonDocument.processMetadata();
-    }
+  process(edmx: Edm.Edmx, options?: object) {
+    const jsonDocument = new JsonDocument(options, edmx);
+    this.data = jsonDocument.processMetadata();
+  }
 
-    requestHandler(format?: string) {
-        return (req: Request, res: Response, next: RequestHandler) => {
-            res.set('OData-Version', '4.0');
-            
-            var data = this.document(format)
-            if (!data['@odata.context']) {
-                var url = req.protocol + '://' + req.get('host') + req.originalUrl.split("?").shift();
-                if(url.slice(-1) !== '/') url += '/'
-                data['@odata.context'] = url + '$metadata';
-            }
-            
-            res.json(data);
-        };
-    }
+  requestHandler(format?: string) {
+    return (req: Request, res: Response, next: RequestHandler) => {
+      res.set('OData-Version', '4.0');
+
+      const data = this.document(format);
+      if (!data['@odata.context']) {
+        let url = req.protocol + '://' + req.get('host') + req.originalUrl.split('?').shift();
+        if (url.slice(-1) !== '/') url += '/';
+        data['@odata.context'] = url + '$metadata';
+      }
+
+      res.json(data);
+    };
+  }
 }
